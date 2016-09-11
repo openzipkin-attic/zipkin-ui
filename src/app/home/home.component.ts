6c48e8c4f97bf2ce31ae01b88adfe6df8da92967
rogeralsing <rogeralsing@gmail.com>
2016-09-11 11:44:23 +0200

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,13 +16,50 @@ export class HomeComponent {
     load() {
         this
             .http
-            .get('http://localhost:9411/api/v1/traces?endTs=1473540618077&lookback=176418077', {})
+            .get('http://localhost:9411/api/v1/traces?endTs=1473586880399&lookback=3600000', {})
             .subscribe(res => {
                 this.traces = <Zipkin.Traces>(res.json());
             });
     }
 
     formatTime(time: number){
-        return new Date(time/1000);
+        let current = new Date();
+        let timestamp = new Date(time/1000);
+        return this.timeDifference(current,timestamp);
     }
+
+    timeDifference(current : any, previous : any) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';
+    }
+
+    else if (elapsed < msPerMonth) {
+        return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';
+    }
+
+    else if (elapsed < msPerYear) {
+        return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';
+    }
+
+    else {
+        return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';
+    }
+}
 }
