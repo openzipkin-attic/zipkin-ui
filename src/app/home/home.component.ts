8d6c317abed56d8a4e8e525273130f0f47e7ab5c
rogeralsing <rogeralsing@gmail.com>
2016-09-11 13:33:46 +0200

    cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Http, Headers, HTTP_PROVIDERS } from '@angular/http';
 import { Inject } from '@angular/core';
 import { Zipkin } from './../zipkin/zipkin';
+import * as moment from 'moment'
 
 @Component({ selector: 'home', template: require('./home.component.html') })
 export class HomeComponent {
@@ -14,6 +15,7 @@ export class HomeComponent {
     }
 
     load() {
+
         this
             .http
             .get('http://localhost:9411/api/v1/traces?endTs=1473586880399&lookback=3600000', {})
@@ -22,44 +24,11 @@ export class HomeComponent {
             });
     }
 
-    formatTime(time: number){
-        let current = new Date();
-        let timestamp = new Date(time/1000);
-        return this.timeDifference(current,timestamp);
+    formatTime(time: number) {
+        return moment(time / 1000).fromNow();
     }
 
-    timeDifference(current : any, previous : any) {
-
-    var msPerMinute = 60 * 1000;
-    var msPerHour = msPerMinute * 60;
-    var msPerDay = msPerHour * 24;
-    var msPerMonth = msPerDay * 30;
-    var msPerYear = msPerDay * 365;
-
-    var elapsed = current - previous;
-
-    if (elapsed < msPerMinute) {
-         return Math.round(elapsed/1000) + ' seconds ago';
+    formatDuration(duration: number) {
+        return moment.duration(duration / 1000).humanize();
     }
-
-    else if (elapsed < msPerHour) {
-         return Math.round(elapsed/msPerMinute) + ' minutes ago';
-    }
-
-    else if (elapsed < msPerDay ) {
-         return Math.round(elapsed/msPerHour ) + ' hours ago';
-    }
-
-    else if (elapsed < msPerMonth) {
-        return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';
-    }
-
-    else if (elapsed < msPerYear) {
-        return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';
-    }
-
-    else {
-        return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';
-    }
-}
 }
