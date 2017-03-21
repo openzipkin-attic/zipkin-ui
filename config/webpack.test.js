b7ec09a4f64709fe2b4998d46c1b625e5fd72655
Andrew Potter <apottere@gmail.com>
2017-03-21 21:29:07 +0800

    Pins to bootstrap 3 and addresses drift to fix the build
    This reverts bootstrap from the alpha, because too much is broken in
    bootstrap itself to be usable (yet).
    
    It also pins versions of dev dependencies so things can build
    consistently across systems.
    
    Fixes #33
    Fixes #32
    Fixes #31
    Fixes #30
    Fixes #29

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -2,27 +2,27 @@ module.exports = {
     devtool: 'inline-source-map',
 
     resolve: {
-        extensions: ['', '.ts', '.js']
+        extensions: ['.ts', '.js']
     },
 
     module: {
         loaders: [
             {
                 test: /\.ts$/,
-                loader: 'ts'
+                loader: 'ts-loader'
             },
             {
                 test: /\.html$/,
-                loader: 'html'
+                loader: 'html-loader'
 
             },
             {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'null'
+                loader: 'null-loader'
             },
             {
                 test: /\.css$/,
-                loader: 'null'
+                loader: 'null-loader'
             }
         ]
     }
